8cd3107a24bd5db9c5b3de3595b538b25858cb1d
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2026-04-15 17:44:17 +0530

    added repo for role

---
+++ UserService/src/main/java/com/userService/UserService/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.userService.UserService.repository;
+
+import com.userService.UserService.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
+
+}
