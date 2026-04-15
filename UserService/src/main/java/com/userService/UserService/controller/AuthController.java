a01978af7798afe90ca683e37989197edfddc3a7
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2026-04-15 19:03:41 +0530

    created controller

---
+++ UserService/src/main/java/com/userService/UserService/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.userService.UserService.controller;
+
+
+import com.userService.UserService.dto.AuthResponse;
+import com.userService.UserService.dto.LoginRequest;
+import com.userService.UserService.dto.RegisterRequest;
+import com.userService.UserService.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // REGISTER
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.registerUser(request));
+    }
+
+    // LOGIN
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(userService.loginUser(request));
+    }
+
+    // REFRESH TOKEN
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refresh(@RequestParam String token) {
+        return ResponseEntity.ok(userService.refreshToken(token));
+    }
+
+//    // LOGOUT
+//    @PostMapping("/logout")
+//    public ResponseEntity<String> logout(@RequestParam String email) {
+//        return ResponseEntity.ok(userService.logoutUser(email));
+//    }
+}
---
+++ UserService/src/main/java/com/userService/UserService/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.userService.UserService.controller;
+
+
+import com.userService.UserService.dto.UserResponse;
+import com.userService.UserService.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //  GET ALL USERS (ADMIN ONLY)
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    //  UPDATE USER STATUS (ADMIN ONLY)
+    @PutMapping("/{id}/status")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> updateStatus(
+            @PathVariable Long id,
+            @RequestParam String status
+    ) {
+        return ResponseEntity.ok(userService.updateUserStatus(id, status));
+    }
+}
