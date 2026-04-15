69a675130037caeb8336ecb6240bbde163645f00
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2026-04-15 17:42:59 +0530

    added service to load user

---
+++ UserService/src/main/java/com/userService/UserService/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.userService.UserService.security;
+
+
+import com.userService.UserService.entity.User;
+import com.userService.UserService.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository repo) {
+        this.userRepository = repo;
+    }
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
+
