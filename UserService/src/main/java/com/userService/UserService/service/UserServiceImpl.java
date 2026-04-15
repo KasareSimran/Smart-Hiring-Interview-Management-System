31680a9a47fc2fcdfa31037b574281ab72758d09
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2026-04-15 18:43:32 +0530

    added the logger

--- UserService/src/main/java/com/userService/UserService/service/UserServiceImpl.java
+++ UserService/src/main/java/com/userService/UserService/service/UserServiceImpl.java
@@ -12,6 +12,8 @@ import com.userService.UserService.entity.User;
 import com.userService.UserService.exception.CustomException;
 import com.userService.UserService.repository.RoleRepository;
 import com.userService.UserService.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,6 +27,10 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(UserServiceImpl.class);
 
     private UserRepository userRepository;
     private RoleRepository roleRepository;
@@ -49,16 +55,22 @@ public class UserServiceImpl implements UserService{
     //register user
     @Override
     public AuthResponse registerUser(RegisterRequest request) {
+
+        logger.info("Register request received for email: {}", request.getEmail());
+
         // Check if user exists
-        userRepository.findByEmail(request.getEmail())
-                .ifPresent(user -> {
-                    throw new CustomException("Email already registered");
-                });
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            logger.error("Registration failed - Email already exists: {}", request.getEmail());
+            throw new CustomException("Email already registered");
+        }
 
         // Assign role
-        String roleName = request.getRole() != null
-                ? request.getRole()
-                : "ROLE_USER";
+        String roleName = request.getRole();
+        if (roleName == null || roleName.isEmpty()) {
+            roleName = "ROLE_USER";
+            logger.info("No role provided. Assigning default role: {}", roleName);
+        }
 
         Role role = roleRepository.findByName(roleName)
                 .orElseThrow(() -> new CustomException("Role not found"));
@@ -75,6 +87,8 @@ public class UserServiceImpl implements UserService{
         user.setStatus(Status.ACTIVE);
 
         userRepository.save(user);
+        logger.info("User registered successfully: {}", user.getEmail());
+
 
 
 
@@ -101,15 +115,37 @@ public class UserServiceImpl implements UserService{
     @Override
     public AuthResponse loginUser(LoginRequest request) {
 
+        logger.info("Login attempt for email: {}", request.getEmail());
+
         // Authenticate
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()
+                    )
+            );
+        } catch (Exception e) {
+            logger.error("Login failed for email: {}", request.getEmail());
+            throw new CustomException("Invalid credentials");
+        }
+
+
+
         User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new CustomException("User not found"));
+                .orElseThrow(() -> {
+                    logger.error("User not found during login: {}", request.getEmail());
+                    return new CustomException("User not found");
+                });
+
+        if (user.getStatus() != Status.ACTIVE) {
+            logger.error("Login blocked - User inactive: {}", request.getEmail());
+            throw new CustomException("User is inactive");
+        }
+
+
+
+        logger.info("Login successful for email: {}", request.getEmail());
 
         String token = jwtProvider.generateAccessToken(user.getEmail());
 
@@ -130,7 +166,11 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<UserResponse> getAllUsers() {
 
+        logger.info("Fetching all users");
+
         List<User> users = userRepository.findAll();
+
+        logger.info("Total users fetched: {}", users.size());
 
         List<UserResponse> responseList = new ArrayList<>();
 
@@ -163,17 +203,27 @@ public class UserServiceImpl implements UserService{
     //update user
     @Override
     public String updateUserStatus(Long userId, String status) {
+
+        logger.info("Updating status for userId: {}", userId);
+
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new CustomException("User not found"));
+                .orElseThrow(() -> {
+                    logger.error("User not found with id: {}", userId);
+                    return new CustomException("User not found");
+                });
 
         try {
             Status newStatus = Status.valueOf(status.toUpperCase());
             user.setStatus(newStatus);
-        } catch (IllegalArgumentException e) {
-            throw new CustomException("Invalid status value");
+        } catch (Exception e) {
+            logger.error("Invalid status provided: {}", status);
+            throw new CustomException("Invalid status");
         }
 
+
         userRepository.save(user);
+
+        logger.info("User status updated successfully for userId: {}", userId);
 
         return "User status updated successfully";
     }
