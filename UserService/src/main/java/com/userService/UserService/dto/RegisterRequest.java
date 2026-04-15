adb5b1686efa125c77cd47a3fbb8379568e620b3
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2026-04-15 16:49:43 +0530

    created and added dto -registerRequest

---
+++ UserService/src/main/java/com/userService/UserService/dto/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.userService.UserService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    // Optional → if not passed, assign default role in service
+    private String role;
+}
